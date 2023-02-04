f533aca853058905ccf14170f37a8fa6422c4096
Kirill <kir.molokoff@yandex.ru>
2023-02-04 23:00:15 +0900

    comLast

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿int number;
+int lower, higher;
+int triesCount = 5;
+int userInput;
+Random rand = new Random();
+
+number = rand.Next(0,101);
+lower = rand.Next(number - 10, number);
+higher = rand.Next(number + 1, number + 10);
+
+Console.WriteLine($"Загадано число от 1 до 100, оно больше чем {lower} , но меньше чем {higher}.");
+Console.WriteLine($"У Вас {triesCount} попыток.");
+
+while(triesCount-- > 0)
+{
+    Console.Write("Ваш ответ:   ");
+    userInput = Convert.ToInt32(Console.ReadLine());
+    if(userInput == number)
+    {
+        Console.WriteLine("Угадали!!! Это действительно число "+ number +".");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Не верно! Попробуйте ещё раз!");
+    }
+}
+if (triesCount < 0)
+{
+Console.WriteLine("Вы проиграли! Это было число "+ number +".");
+}
